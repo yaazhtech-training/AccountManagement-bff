c9d35c4a5eb86bf8394d373a4262928d240c69d6
PRAVEENM98765 <praveenmohan3010@gmail.com>
2025-07-11 15:43:27 +0530

    database connection

---
+++ src/main/resources/ DynamoDbConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.AP_SOUTH_1) // choose your region
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
